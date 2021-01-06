46802fcfeca500a09e6931e698f370cd992fb042
Chanuga <Chanugatharindu1995@gmail.com>
2021-01-06 23:35:03 +0530

    Updates

---
+++ precache-manifest.666da226cd3e78ca27d574dc922f0b16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd7d3b0b5ba651f36b17ac19acab269c",
+    "url": "/Food-Sense/index.html"
+  },
+  {
+    "revision": "8492dbc84aea53ad90f9",
+    "url": "/Food-Sense/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b2fe8d07544980441d2c",
+    "url": "/Food-Sense/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8492dbc84aea53ad90f9",
+    "url": "/Food-Sense/static/js/2.151abfd3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Food-Sense/static/js/2.151abfd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2fe8d07544980441d2c",
+    "url": "/Food-Sense/static/js/main.ae2bdb44.chunk.js"
+  },
+  {
+    "revision": "5e0155b4b5af64c10f35",
+    "url": "/Food-Sense/static/js/runtime-main.aed47445.js"
+  }
+]);
